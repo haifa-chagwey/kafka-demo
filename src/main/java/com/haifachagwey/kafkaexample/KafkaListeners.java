808883bb414941e0c37acbacca293a4c288d12f0
haifachagwey@gmail.com <haifachagwey@gmail.com>
2024-12-06 22:24:27 +0100

    kafka example

---
+++ src/main/java/com/haifachagwey/kafkaexample/KafkaListeners.java
@@ -0,0 +1,17 @@
+package com.haifachagwey.kafkaexample;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "amigoscode",
+            groupId = "groupId"
+//            containerFactory = "messageFactory"
+    )
+    void listener(String data) {
+        System.out.println("Listener received: " + data + "🎉");
+    }
+}
---
+++ src/main/java/com/haifachagwey/kafkaexample/Message.java
@@ -0,0 +1,6 @@
+package com.haifachagwey.kafkaexample;
+
+import java.time.LocalDateTime;
+
+public record Message(String message, LocalDateTime created) {
+}
